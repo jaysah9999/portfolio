cceab9655fb8ba0785f4b46131281da4692aa5a7
jaysah9999 <jaykrsah23@gmail.com>
2026-03-29 17:25:20 +0530

    Add AOS initialization and typed effect

---
+++ Script.js
@@ -0,0 +1,10 @@
+// Initialize AOS animation
+AOS.init();
+
+// Typed effect
+var typed = new Typed(".typed", {
+  strings: ["Designer", "Developer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
